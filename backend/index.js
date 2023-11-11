9996ec7cfde370f2b14a48c77f46bfebb49fefdc
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-11 16:08:29 +0530

    project setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Project Working Fine !!!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Server is running at ${PORT}`);
+});
