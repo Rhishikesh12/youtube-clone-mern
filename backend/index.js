d5991a3bf62a5f1e367cf78c3db7e41cbe103995
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-11-11 16:39:56 +0530

    Connecter DB and Created Models

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
-const express = require("express");
+// Imports
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// App
 const app = express();
 
+// DB Connection
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => {
+		console.log("Database Connected Successfully");
+	})
+	.catch((err) => console.log(err));
+
+// Routes
 app.get("/", (req, res) => {
 	res.send("Project Working Fine !!!");
 });
 
+// Server Listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server is running at ${PORT}`);
